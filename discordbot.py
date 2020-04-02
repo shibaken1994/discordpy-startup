1fc95cfb3b4345f1d8eee6606459e15bc2b7e6fc
shibaken1994 <62886462+shibaken1994@users.noreply.github.com>
2020-04-03 04:38:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,10 @@
+  
 from discord.ext import commands
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
-token = ['DISCORD_BOT_TOKEN']
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
@@ -17,11 +18,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-    
-@bot.command()
-async def ss(ctx):
-    await ctx.send('pong')
 
-    
-  
 bot.run(token)
